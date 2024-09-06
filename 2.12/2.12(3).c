9a51c9f323777944d1cbcd9206c1937e33742a0f
Sorakari <sorakaridaisukidayo@gmail.com>
2024-09-06 11:45:29 +0800

    第二章

---
+++ 2.12/2.12(3).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int days,daysperyear;
+
+    daysperyear = 365;
+    days = 19 * daysperyear;
+
+    printf("You have live 19 years which means %d days", days);
+
+    return 0;
+
+}
---
+++ 2.12/2.12(6).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int toes;
+    toes = 10;
+
+    printf("Toes = %d\n", toes);
+    printf("Double toes = %d\n",toes * 2);
+    printf("Toes squared = %d",toes * toes);
+
+    return 0;
+
+}
